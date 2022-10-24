59b609725fd53d008816ef8a19748008705c494f
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-10-24 23:26:57 +0400

    Gitignore fixes

--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A function that sums up two numbers and returns the result.
- *
- * @param a
- * @param b
- */
-export declare function sum(a: number, b: number): number;
--- dist/vite-vue-starter-slave.d.ts
+++
@@ -1,3 +0,0 @@
-import SlaveComponent from "./components/SlaveComponent.vue";
-export { SlaveComponent };
-export * as utils from "./utils";
