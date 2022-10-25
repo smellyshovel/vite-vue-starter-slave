04f25b819b2b9c67f262cd8c1dd4420228e2cd8d
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-10-25 18:23:37 +0400

    No need in safelist

--- src/components/SlaveComponent.vue
+++ src/components/SlaveComponent.vue
@@ -3,10 +3,7 @@
     <h1>This is the Slave Lib's Component</h1>
     <p class="bg-red-500">It can use Tailwind's default styles (bg-red-500)</p>
     <p class="bg-slave">As well as its `tailwind.config.cjs`'s custom classes (bg-slave)</p>
-    <p class="bg-master">
-      And even the master's custom classes (bg-master). But only if these classes are either used in master itself, or
-      "safelisted" in master's tailwind.config.cjs
-    </p>
+    <p class="bg-master">And even the master's custom classes (bg-master)</p>
     <div>A typed prop (string): {{ greeting }}</div>
   </div>
 </template>
